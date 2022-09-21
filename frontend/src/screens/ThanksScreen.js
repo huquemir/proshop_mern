617de7ab873e8f7ba93d6d87fa29511bc832125e
huquemir <huque.mir@gmail.com>
2022-09-21 00:00:04 -0400

    added iframe map

--- frontend/src/screens/ThanksScreen.js
+++ frontend/src/screens/ThanksScreen.js
@@ -28,20 +28,28 @@ const ThanksScreen = () => {
     };
    
     return (
-      <>
+      <div>
       {
       list &&
       list.map((item, index) => (
         <div style={{backgroundColor:'lightblue', margin:'20px 25%', textAlign:'center', fontSize:'40px'}}
+          
           onDragStart={(e) => dragStart(e, index)}
           onDragEnter={(e) => dragEnter(e, index)}
           onDragEnd={drop}
           key={index}
           draggable>
             {item}
-        </div>
+          </div>
+        
         ))}
-      </>
+       <iframe id="inlineFrameExample"
+    title="Inline Frame Example"
+    width="300"
+    height="200"
+    src="https://www.openstreetmap.org/export/embed.html?bbox=-0.004017949104309083%2C51.47612752641776%2C0.00030577182769775396%2C51.478569861898606&layer=mapnik">
+</iframe>
+      </div>
     );
 }
 
